e7144ee3e20c4a53128b8a39454b8ebbd0008b2c
unknown <gabemarredondo@gmail.com>
2022-09-08 12:58:11 -0700

    added link and got data from 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,26 @@
 var searchEl = document.querySelector("#search");
 var searchEl = document.querySelector("#search");
+// citys = "los angeles"
+
+
+
 
 searchEl.addEventListener("click", fetchWeather);
 function fetchWeather(event) {
-  removeprevious();
   var citys = document.querySelector(".citys").value;
+  removeprevious();
   if (!citys) {
     alert("please enter a city");
     return;
   }
   console.log(citys);
-  window.localStorage.setItem('1', citys);
+  window.localStorage.setItem("1", citys);
   var url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     citys +
     "&appid=d7bb72a0ceb5ca152a17cec919b7c2a6&units=imperial";
 
-    fetch(url)
+  fetch(url)
     .then(function (response) {
       return response.json();
     })
@@ -27,35 +31,42 @@ function fetchWeather(event) {
       var place = document.createElement("h2");
       place.textContent = data.name;
       currentResults.append(place);
-      place.classList.add('city')
+      place.classList.add("city");
       // create p for temp
       var tempeture = document.createElement("p");
       tempeture.textContent = "Temp: " + data.main.temp + "F";
       currentResults.append(tempeture);
-      tempeture.classList.add("details")
+      tempeture.classList.add("details");
       // create p for wind
       var wind = document.createElement("p");
       wind.textContent = "Wind: " + data.wind.speed + "mph";
       currentResults.append(wind);
-      wind.classList.add("details")
+      wind.classList.add("details");
       // create p for humidity
       var humidity = document.createElement("p");
       humidity.textContent = "Humidity: " + data.main.humidity + "%";
       currentResults.append(humidity);
-      humidity.classList.add("details")
+      humidity.classList.add("details");
+      fetchForecast();
     });
-  }
-function recentsearch() {
-    var historyEl = document.querySelector("#search-history");
-    var search1 = window.localStorage.getItem("1");
-    var firstSearch = document.createElement("button");
-    firstSearch.classList.add("lastsearch")
-    firstSearch.textContent = search1;
-    historyEl.append(firstSearch);
 }
-function removeprevious(){
+
+
+
+function recentsearch() {
+  var historyEl = document.querySelector("#search-history");
+  var search1 = window.localStorage.getItem("1");
+  var firstSearch = document.createElement("button");
+  firstSearch.classList.add("lastsearch");
+  firstSearch.textContent = search1;
+  historyEl.append(firstSearch);
+}
+
+
+
+function removeprevious() {
   var details1 = document.querySelector(".details");
-  if (details1){
+  if (details1) {
     var cityRemove = document.querySelector(".city");
     cityRemove.remove();
     var details1 = document.querySelector(".details");
@@ -67,4 +78,24 @@ function removeprevious(){
   }
 }
 
+
+
+function fetchForecast(query) {
+  var citys = document.querySelector(".citys").value;
+  var url =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" + citys + "&appid=d7bb72a0ceb5ca152a17cec919b7c2a6&units=imperial";
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data.list[1])
+      console.log(data.list[9])
+      console.log(data.list[17])
+      console.log(data.list[25])
+      console.log(data.list[33])
+    });
+}
 recentsearch();
